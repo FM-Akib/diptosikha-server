28d48c9fccf6fcab607b2ec784926e66be4f70ba
FM-Akib <muntasirakib08@gmail.com>
2024-03-16 05:43:13 +0600

    user data updated

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ async function run() {
 const UserCollection = client.db('diptosikhaDB').collection('users');
 
 
-    
+  //user data handle
     app.get('/users', async(req, res) =>{
      const cursor = UserCollection.find()
      const result = await cursor.toArray()
@@ -41,6 +41,41 @@ const UserCollection = client.db('diptosikhaDB').collection('users');
         const result = await UserCollection.findOne(query);
         res.send(result);
        })
+
+    app.post('/users',async(req, res)=>{
+      const user = req.body;
+      const result = await UserCollection.insertOne(user);
+      res.send(result);
+    })
+
+    app.put('/users/:id',async(req, res)=>{
+      const id= req.params.id;
+      const user = req.body;
+      const filter = {_id: new ObjectId(id)}
+      const options = {upsert: true}
+      const updatedUser={
+        $set:{
+          name:user.name,
+          email:user.email,
+          img:user.img,
+          designation:user.designation,
+          mobile:user.mobile,
+          bloodGroup:user.bloodGroup,
+          facebook:user.facebook,
+          education:user.education,
+          lastDonate:user.lastDonate,
+        }
+      }
+      const result = await UserCollection.updateOne(filter,updatedUser,options);
+      res.send(result);
+
+    })
+
+
+
+
+
+
 
 
     await client.connect();
